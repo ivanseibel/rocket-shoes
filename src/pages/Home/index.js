e802d5cb28134817b1752f6367b46de785d760b2
Ivan Seibel <ivanseibel@gmail.com>
2020-04-07 12:51:26 -0300

    Applying styles do Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,95 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
+import shoes from '../../assets/images/shoes.jpg';
 
 export default function Home() {
-  return <p>Home</p>;
+  return (
+    <ProductList>
+      <li>
+        <img src={shoes} alt="Tênis" />
+        <strong>Running shoes</strong>
+        <span>$39.00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+
+      <li>
+        <img src={shoes} alt="Tênis" />
+        <strong>Running shoes</strong>
+        <span>$39.00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+
+      <li>
+        <img src={shoes} alt="Tênis" />
+        <strong>Running shoes</strong>
+        <span>$39.00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+
+      <li>
+        <img src={shoes} alt="Tênis" />
+        <strong>Running shoes</strong>
+        <span>$39.00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+
+      <li>
+        <img src={shoes} alt="Tênis" />
+        <strong>Running shoes</strong>
+        <span>$39.00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+
+      <li>
+        <img src={shoes} alt="Tênis" />
+        <strong>Running shoes</strong>
+        <span>$39.00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
