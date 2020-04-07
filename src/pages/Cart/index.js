794288717af26ff46e79da9d0b3c231e82a48e6f
Ivan Seibel <ivanseibel@gmail.com>
2020-04-07 16:18:49 -0300

    Applying styles to Cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,66 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
+import shoes from '../../assets/images/shoes.jpg';
 
 export default function Cart() {
-  return <p>Cart</p>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUCT</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src={shoes} alt="Shoes" />
+            </td>
+            <td>
+              <strong>Running Shoes</strong>
+              <span>$129.90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>$258.80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Checkout</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>$1,920.28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
