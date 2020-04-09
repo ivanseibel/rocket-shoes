bc500a37ab88c97aefbefc088559f481500db42c
Ivan Seibel <ivanseibel@gmail.com>
2020-04-09 18:55:18 -0300

    Configuring react-navigation-stack

---
+++ mobile/src/pages/Cart/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default class Cart extends Component {
+  state = {
+    items: [],
+  };
+
+  render() {
+    return <Text>Cart</Text>;
+  }
+}
---
+++ mobile/src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  render() {
+    return <Text>Home</Text>;
+  }
+}
