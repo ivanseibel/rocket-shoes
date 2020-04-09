f5855d0b8475ddbc5f50492c9496a32d03d2620b
Ivan Seibel <ivanseibel@gmail.com>
2020-04-09 14:20:07 -0300

    Implementing prop-types validation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import { Container, Cart } from './styles';
 import logo from '../../assets/images/logo.svg';
@@ -23,6 +24,10 @@ function Header({ cartSize }) {
     </Container>
   );
 }
+
+Header.propTypes = {
+  cartSize: PropTypes.number.isRequired,
+};
 
 export default connect((state) => ({
   cartSize: state.cart.length,
