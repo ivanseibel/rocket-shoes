325317ea0735b7c7f75dcc43592d43ac8300283b
Ivan Seibel <ivanseibel@gmail.com>
2020-04-09 17:19:41 -0300

    Reatotron integrated and configurated

--- mobile/index.js
+++ mobile/index.js
@@ -2,8 +2,15 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './src';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+if (__DEV__) {
+  import('./src/config/ReactotronConfig').then(() =>
+    // eslint-disable-next-line no-console
+    console.log('Reactotron Configured')
+  );
+}
