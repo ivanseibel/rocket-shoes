b76dee5d29eeb300ab71ad41b1dde7e5bea5a5bf
Ivan Seibel <ivanseibel@gmail.com>
2020-04-08 22:06:41 -0300

    Showing product amount added to cart

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -31,6 +31,7 @@ class Home extends Component {
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
     return (
       <ProductList>
         {products.map((product) => (
@@ -46,7 +47,8 @@ class Home extends Component {
               }}
             >
               <div>
-                <MdAddShoppingCart size={16} color="#fff" /> 3
+                <MdAddShoppingCart size={16} color="#fff" />{' '}
+                {amount[product.id] || 0}
               </div>
 
               <span>ADD TO CART</span>
@@ -58,9 +60,16 @@ class Home extends Component {
   }
 }
 
+const mapStateToProps = (state) => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
+
 // Send CartActions to props
 const mapDispatchToProps = (dispatch) =>
   bindActionCreators(CartActions, dispatch);
 
 // First parameter is null because there is no mapStateToProps
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
