396970bba9852324215b3204fa07773420c1fdcc
Ivan Seibel <ivanseibel@gmail.com>
2020-04-10 14:07:25 -0300

    Install and configure styled-components

--- mobile/src/components/Header/index.js
+++ mobile/src/components/Header/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import { Container, Logo, Wrapper, BasketContainer, ItemCount } from './styles';
 
-export default function Header() {
-  return <Text>Header</Text>;
+export default function Header(navigation) {
+  console.tron.log({ navigation });
+  return (
+    <Wrapper>
+      <Container>
+        <Logo />
+        <BasketContainer onPress={() => navigation.navigate('Cart')}>
+          <Icon name="shopping-basket" color="#FFF" size={24} />
+          <ItemCount>0</ItemCount>
+          {/* <ItemCount>{cartSize || 0}</ItemCount> */}
+        </BasketContainer>
+      </Container>
+    </Wrapper>
+  );
 }
--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,15 +1,65 @@
 import React, { Component } from 'react';
+import { FlatList, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Text } from 'react-native';
-
-// import { Container } from './styles';
+import {
+  Container,
+  ProductBox,
+  ProductImg,
+  ProductTitle,
+  ProductPrice,
+  AddButton,
+  AddText,
+} from './styles';
 
 export default class Home extends Component {
   state = {
-    products: [],
+    products: [
+      {
+        id: 1,
+        title: 'Tênis de Caminhada Leve Confortável',
+        price: 179.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+      },
+      {
+        id: 2,
+        title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
+        price: 139.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+      },
+      {
+        id: 3,
+        title: 'Tênis Adidas Duramo Lite 2.0',
+        price: 219.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg',
+      },
+    ],
   };
 
   render() {
-    return <Text>Home</Text>;
+    const { products } = this.state;
+    return (
+      <Container>
+        <FlatList
+          horizontal
+          data={products}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => (
+            <ProductBox>
+              <ProductImg source={{ uri: item.image }} />
+              <ProductTitle>{item.title}</ProductTitle>
+              <ProductPrice>{item.price}</ProductPrice>
+              <AddButton>
+                <Icon name="add-shopping-cart" />
+                <AddText>ADD TO CART</AddText>
+              </AddButton>
+            </ProductBox>
+          )}
+        />
+      </Container>
+    );
   }
 }
