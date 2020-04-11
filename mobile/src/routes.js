3f094ac747f0bb6da06998cb6384b8b69141525b
Ivan Seibel <ivanseibel@gmail.com>
2020-04-11 10:09:38 -0300

    Getting api product details

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, TransitionSpecs } from '@react-navigation/stack';
 
 import Home from './pages/Home';
 import Cart from './pages/Cart';
