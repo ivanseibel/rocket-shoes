5ed52aa83d98fe96fc0ea35dbca91dc5007366eb
Ivan Seibel <ivanseibel@gmail.com>
2020-04-13 17:31:45 -0300

    Changing Home class component to function

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -9,56 +9,54 @@ import api from '../../services/api';
 import { formatPrice } from '../../util/format';
 import * as CartActions from '../../store/modules/cart/actions';
 
-class Home extends Component {
-  state = {
-    products: [],
-  };
+function Home({ amount, addToCartRequest }) {
+  const [products, setProducts] = useState([]);
 
-  async componentDidMount() {
-    const response = await api.get('products');
+  // Overriding the componentDidMount method
+  useEffect(() => {
+    async function loadProducts() {
+      const response = await api.get('products');
 
-    const data = response.data.map((product) => ({
-      ...product,
-      formattedPrice: formatPrice(product.price),
-    }));
-    this.setState({ products: data });
-  }
+      const data = response.data.map((product) => ({
+        ...product,
+        formattedPrice: formatPrice(product.price),
+      }));
 
-  handleAddProduct = (id) => {
-    const { addToCartRequest } = this.props;
+      setProducts(data);
+    }
 
+    loadProducts();
+  }, []);
+
+  function handleAddProduct(id) {
     addToCartRequest(id);
-  };
-
-  render() {
-    const { products } = this.state;
-    const { amount } = this.props;
-    return (
-      <ProductList>
-        {products.map((product) => (
-          <li key={String(product.id)}>
-            <img src={product.image} alt={product.title} />
-            <strong>{product.title}</strong>
-            <span>{product.formattedPrice}</span>
-
-            <button
-              type="button"
-              onClick={() => {
-                this.handleAddProduct(product.id);
-              }}
-            >
-              <div>
-                <MdAddShoppingCart size={16} color="#fff" />{' '}
-                {amount[product.id] || 0}
-              </div>
-
-              <span>ADD TO CART</span>
-            </button>
-          </li>
-        ))}
-      </ProductList>
-    );
   }
+
+  return (
+    <ProductList>
+      {products.map((product) => (
+        <li key={String(product.id)}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.formattedPrice}</span>
+
+          <button
+            type="button"
+            onClick={() => {
+              handleAddProduct(product.id);
+            }}
+          >
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" />{' '}
+              {amount[product.id] || 0}
+            </div>
+
+            <span>ADD TO CART</span>
+          </button>
+        </li>
+      ))}
+    </ProductList>
+  );
 }
 
 Home.propTypes = {
